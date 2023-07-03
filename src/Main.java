d7126cc972763993e97b03a7e9502eefc81c9d16
suviga sergey <79221884011@yandex.ru>
2023-07-03 19:47:08 +0500

    HW1.1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        //task2();
+        // task3();
+        //task4();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+
+    }
+}
