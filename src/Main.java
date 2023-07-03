233f4318d2eba2ebfb5a8a3f538bce650fe66d78
suviga sergey <79221884011@yandex.ru>
2023-07-03 20:47:15 +0500

    HW1.2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,50 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
         task1();
-        //task2();
-        // task3();
+        task2();
+        task3();
         //task4();
     }
 
     public static void task1() {
         System.out.println("Задача 1");
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+        System.out.printf("ФИО сотрудника — %s", fullName);
+        System.out.println();
 
     }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = String.format("%s %s %s", lastName.toUpperCase(),
+                firstName.toUpperCase(),
+                middleName.toUpperCase());
+        System.out.println(fullName);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+
+        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+        char incorrektSyambol = 'ё';
+        char correktSyambol = 'е';
+        String currectFullName = fullName.replace(incorrektSyambol, correktSyambol);
+        System.out.println("Данные ФИО сотрудника " + " - " +currectFullName);
+    }
+
 }
